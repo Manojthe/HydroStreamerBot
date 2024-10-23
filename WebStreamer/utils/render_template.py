c7f927647dd186fb251921b16aca7fae7e908a58
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-10-23 09:16:17 +0530

    Update render_template.py

--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -1,4 +1,5 @@
 # This file is a part of FileStreamBot
+
 import jinja2
 import aiohttp
 import aiofiles
@@ -11,30 +12,25 @@ db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 async def render_page(db_id):
     file_data=await db.get_file(db_id)
     src = urllib.parse.urljoin(Var.URL, f'dl/{file_data["_id"]}')
+    template_file="WebStreamer/template/req.html"
+    file_size = humanbytes(int(u.headers.get('Content-Length')))
+    tag = (file_data['mime_type']).split('/')[0].strip()
+
     if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
-        async with aiofiles.open('WebStreamer/template/req.html') as r:
             heading = 'Watch {}'.format(file_data['file_name'])
-            tag = (file_data['mime_type']).split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data['file_name'], src)
     elif str((file_data['mime_type']).split('/')[0].strip()) == 'audio':
-        async with aiofiles.open('WebStreamer/template/req.html') as r:
             heading = 'Listen {}'.format(file_data['file_name'])
-            tag = (file_data['mime_type']).split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data['file_name'], src)
     else:
-        async with aiofiles.open('WebStreamer/template/dl.html') as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    heading = 'Download {}'.format(file_data['file_name'])
-                    file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    with open(template_file) as f:
-        template = jinja2.Template(f.read())
+        template_file="WebStreamer/template/dl.html"
+        heading = 'Download {}'.format(file_data['file_name'])
 
-    file_name = file_data.file_name.replace("_", " ")
-
-    return template.render(
-        file_name=file_name,
-        file_url=src,
-        file_size=file_size,
-        file_unique_id=file_data.unique_id,
-    )
+    with open(template_file) as f: 
+        template = jinja2.Template(f.read()) 
+        return template.render( 
+            file_name=file_data['file_name'], 
+            file_url=src, 
+            file_size=file_size, 
+            file_unique_id=file_data['unique_id'], 
+            heading=heading,
+            tag=tag
+        )
